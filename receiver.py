27fbe89f844292477f18409b1bf6493921cba6be
Connie Chan <cc.conch@gmail.com>
2012-04-22 09:21:15 -0400

    correct the handshake in the python receiver

--- receiver.py
+++ receiver.py
@@ -8,7 +8,7 @@ group_size = 9 # a group is a sign byte plus the normal bytes that follow it
 ser = serial.Serial("/dev/tty.usbserial-A80090rN", 19200)
 f = open("camera_data.txt", "w")
 time.sleep(2) # need to wait a bit because it takes time to open serial port
-ser.write("hello") # handshake to tell Arduino to send EEPROM contents
+ser.write("hello\n") # handshake to tell Arduino to send EEPROM contents
 
 num_of_photos = (eeprom_size / group_size) * (group_size - 1) + (eeprom_size - (eeprom_size / group_size) * group_size - 1)
 for i in range(num_of_photos):
