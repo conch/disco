6c055bb1ca562cfa1325e9cd2709a03da1a67ec3
Connie Chan <cc.conch@gmail.com>
2012-04-29 01:38:24 -0400

    use camera data file to perform distortion correction by modifying Lightroom's SQLite database

--- receiver.py
+++ receiver.py
@@ -6,7 +6,7 @@ eeprom_size = 1024
 group_size = 9 # a group is a sign byte plus the normal bytes that follow it
 
 ser = serial.Serial("/dev/tty.usbserial-A80090rN", 19200)
-f = open("camera_data.txt", "w")
+f = open("/tmp/disco_camera_data.txt", "w")
 time.sleep(2) # need to wait a bit because it takes time to open serial port
 ser.write("hello\n") # handshake to tell Arduino to send EEPROM contents
 
--- write_to_sqlite.py
+++ write_to_sqlite.py
@@ -1,7 +1,9 @@
-import commands
-import re
-import sqlite3
-import sys
+import commands, re, sqlite3, sys
+
+# close Lightroom
+term_out = commands.getstatusoutput("ps -axo pid,comm | grep '/Applications/Adobe Photoshop Lightroom 4.app/Contents/MacOS/Adobe Photoshop Lightroom 4'")[1]
+pid = re.search("(\d+)\s.+", term_out).group(1)
+commands.getstatusoutput("kill " + pid)
 
 cat_path = sys.argv[1]
 photo_uuids = ""
@@ -9,21 +11,27 @@ for photo_uuid in sys.argv[2:]:
   if photo_uuids != "":
     photo_uuids += str(", ")
   photo_uuids += "'" + photo_uuid + "'"
-query = "select text from Adobe_imageDevelopSettings join Adobe_images on Adobe_imageDevelopSettings.image = Adobe_images.id_local"
+query = "select image, text, id_global from Adobe_imageDevelopSettings join Adobe_images on Adobe_imageDevelopSettings.image = Adobe_images.id_local"
 if photo_uuids != "":
   query += " where id_global in (" + photo_uuids + ")"
+query += " order by Adobe_images.captureTime asc"
 
-f = open("/Users/conch/hopeful.txt", "w")
 conn = sqlite3.connect(cat_path)
 db = conn.cursor()
 db.execute(query)
-for image in db.fetchall():
-  f.write(image[0])
-  f.write("\n")
-f.close()
+uq = "update Adobe_imageDevelopSettings set text = '"
+with open("/tmp/disco_camera_data.txt", "r") as data:
+  for row in db.fetchall():
+    d = data.readline()
+    d = d[:len(d) - 2]
+    text = row[1]
+    m = re.search("PerspectiveVertical = (.+),", text)
+    start = m.start(1)
+    end = m.end(1)
+    db.execute(uq + text[:start] + d + text[end:] + "' where image = " + str(row[0]))
+conn.commit()
+db.close()
+conn.close()
 
-term_out = commands.getstatusoutput("ps -axo pid,comm | grep '/Applications/Adobe Photoshop Lightroom 4.app/Contents/MacOS/Adobe Photoshop Lightroom 4'")[1]
-pid = re.search("(\d+)\s.+", term_out).group(1)
-# restart Lightroom
-commands.getstatusoutput("kill " + pid)
+# start Lightroom
 commands.getstatusoutput("open /Applications/Adobe\ Photoshop\ Lightroom\ 4.app")
